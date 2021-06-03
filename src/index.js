d6bcfd0900dd6f425dc96ec4a21e153f08406d1d
Pedro Henrique <pedrohenrique.ufrj@gmail.com>
2021-06-03 19:07:31 -0300

    Commit inicial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ConversorMoedas from './conversor-moedas';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ConversorMoedas/>,
   document.getElementById('root')
 );
 
@@ -15,3 +14,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
